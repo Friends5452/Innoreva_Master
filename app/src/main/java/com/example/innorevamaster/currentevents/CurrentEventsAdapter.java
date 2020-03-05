72378e06c9406b041354394d3ceca05920849a7b
Harjot <asingh.harjot@gmail.com>
2020-03-06 00:19:17 +0530

    commit after adding adapter1

--- app/src/main/java/com/example/innorevamaster/currentevents/CurrentEventsAdapter.java
+++ app/src/main/java/com/example/innorevamaster/currentevents/CurrentEventsAdapter.java
@@ -1,11 +1,13 @@
 package com.example.innorevamaster.currentevents;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.innorevamaster.R;
@@ -27,11 +29,16 @@ public class CurrentEventsAdapter extends RecyclerView.Adapter<CurrentEventsAdap
     @NonNull
     @Override
     public CurrentEventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.current_event_card, parent, false);
+
+        return new CurrentEventViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CurrentEventViewHolder holder, int position) {
+
+        holder.descriptionTextView.setText(currentEventsArrayList.get(position).eventDescriptionString);
 
     }
 
@@ -43,9 +50,11 @@ public class CurrentEventsAdapter extends RecyclerView.Adapter<CurrentEventsAdap
     public static class CurrentEventViewHolder extends RecyclerView.ViewHolder {
         CircleImageView eventImage;
         TextView descriptionTextView;
+        CardView eventCardView;
 
         public CurrentEventViewHolder(@NonNull View itemView) {
             super(itemView);
+            eventCardView=itemView.findViewById(R.id.event_card_view);
             eventImage=itemView.findViewById(R.id.current_event_image);
             descriptionTextView=itemView.findViewById(R.id.current_event_description);
         }
--- app/src/main/java/com/example/innorevamaster/currentevents/MainActivity.java
+++ app/src/main/java/com/example/innorevamaster/currentevents/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.innorevamaster;
+package com.example.innorevamaster.currentevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.innorevamaster.R;
 
 public class MainActivity extends AppCompatActivity {
 
